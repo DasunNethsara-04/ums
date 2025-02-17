c7b2b04993946760cb4295032364370add36d996
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-17 10:31:34 +0530

    add role model with enumeration for user roles

---
+++ backend/models/roles.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, Sequence, Enum
+from database import Base
+from enum import Enum as PyEnum
+
+
+class RoleEnum(PyEnum):
+    ADMIN = "admin"
+    MAINTAINER = "maintainer"
+    MODERATOR = "moderator"
+    USER = "user"
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, Sequence("role_id_seq"), primary_key=True, index=True)
+    role = Column(Enum(RoleEnum))
+
+    def to_dict(self) -> dict[str, str]:
+        return {
+            "id": self.id,
+            "name": self.role
+        }
+    
+    # feed automatically some data when the table was created
