8f53a6b4894684e21698623754df86a2cb744e44
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 13:56:26 +0530

    feat: add startup event to initialize database and seed roles

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from database.database import Base, get_db, engine
+from models import User, Role
+from factory import RoleFactory
+from database import DBSeeder
 
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def on_startup() -> None:
+    Base.metadata.create_all(bind=engine)
+    db = next(get_db())
+    DBSeeder(db).seed(RoleFactory().create())
+
+
 @app.get("/")
-async def index():
+async def index() -> dict[str, str]:
     return {"message": "Hello, World!"}
