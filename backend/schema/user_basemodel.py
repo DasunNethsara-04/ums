7580b45ae26c3c4c19ab5aba0c63aed789bc9e6d
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 23:32:51 +0530

    feat: add role_id and created_at fields to UserBaseModel

--- backend/schema/user_basemodel.py
+++ backend/schema/user_basemodel.py
@@ -4,11 +4,13 @@ from typing import Optional
 
 class UserBaseModel(BaseModel):
     id: Optional[int] = None
+    role_id: Optional[int] = None
     username: str
     email: str
     full_name: Optional[str] = None
     disabled: Optional[bool] = None
     hashed_password: str | None = None
+    created_at: Optional[str] = None
 
     def __str__(self) -> str:
         return self.username
