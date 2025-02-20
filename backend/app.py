5c371a531f3cf08e3d2fc9ba18807e94faa34214
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 17:22:38 +0530

    feat: add CORS and RateLimitMiddleware to FastAPI application

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.database import Base, get_db, engine
 from models import User, Role
 from factory import RoleFactory
 from database import DBSeeder
+from routes import auth
+from middleware import RateLimitMiddleware
 
+# initialize the main FastAPI application
 app = FastAPI()
+
+# include routes
+app.include_router(auth.router)
+
+# middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(RateLimitMiddleware)
 
 
 @app.on_event("startup")
