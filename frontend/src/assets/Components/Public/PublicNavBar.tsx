9970c5d61b4f7c245b5bf410c5f6440f017f33e1
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 20:08:32 +0530

    feat: enhance PublicNavBar with Bootstrap styling and navigation links

--- frontend/src/assets/Components/Public/PublicNavBar.tsx
+++ frontend/src/assets/Components/Public/PublicNavBar.tsx
@@ -1,6 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 const PublicNavBar = () => {
     return (
-        <div>PublicNavBar</div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand as={Link} to="/">UMS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto"></Nav>
+                    <a className='btn btn-sm btn-primary' as={Link} href='/login'>Login</a>
+                    <a className='ms-2 btn btn-sm btn-outline-secondary' as={Link} href='/register'>Register</a>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
