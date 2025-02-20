5150a9a39648f0cb3585beadb09ef1a1f9b83439
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 13:55:16 +0530

    fix: specify type for engine and session in database module

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,10 +1,10 @@
 from typing import Any, Generator
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm.session import Session
 from sqlalchemy_utils import database_exists, create_database
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/ums")
+engine: Engine = create_engine("mysql+pymysql://root@localhost:3306/ums")
 if not database_exists(engine.url):
     create_database(engine.url)
 
@@ -12,11 +12,9 @@ session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-
-def get_db() -> Generator[Session, Any, None]:
-    db = session()
+def get_db() -> Generator[sessionmaker, Any, None]:
+    db: Session = session()
     try:
         yield db
     finally:
         db.close()
-
