7a50ef19b446250a109dcfd12b5688f33f21d4dd
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 17:34:01 +0530

    feat: integrate React Router and Bootstrap into main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
 import App from './App.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
