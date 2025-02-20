86913e82b9f0f7a70dab0136aa624beb30dddb4d
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-20 20:14:31 +0530

    feat: implement login form with Bootstrap styling in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,40 @@
+import { Button, Card, Form } from "react-bootstrap"
+import PublicNavBar from "./assets/Components/Public/PublicNavBar"
+
 const App = () => {
+  const handleSubmit = async (event: any) => {
+    // Prevent the form from reloading the page
+    event.preventDefault();
+  }
+
+
   return (
     <>
-      <h1>Hello</h1>
+      <PublicNavBar />
+      <div className='position-absolute top-50 start-50 translate-middle'>
+        <Card style={{ width: "50rem" }}>
+          <Card.Header>
+            <Card.Title className="text-center" as="h1">Login</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Form method='post' onSubmit={handleSubmit}>
+              <Form.Group className='mb-3'>
+                <Form.Label>Username</Form.Label>
+                <Form.Control type='text' name='username' required />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" required />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <div className="d-grid gap-2">
+                  <Button type='submit' variant='outline-primary'>Login</Button>
+                </div>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   )
 }
