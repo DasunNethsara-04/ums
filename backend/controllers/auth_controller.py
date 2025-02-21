26df635cf6f42ea66747925e71b41b95928e167f
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-21 21:49:23 +0530

    feat: add method to retrieve current logged-in user from JWT token

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -1,12 +1,14 @@
 from schema import UserBaseModel, TokenBaseModel
 from datetime import datetime, timedelta
-from typing import Final
+from typing import Final, Any
 from database import Session
 from jose import jwt
 from models import User
 from passlib.context import CryptContext
 from exception import HttpBadRequest
 from controllers import UserController
+from jose.exceptions import JWTError
+from exception.unauthorized import HttpUnauthorized
 
 ACCESS_TOKEN_EXPIRE_MINUTES: Final[int] = 30
 
@@ -15,6 +17,17 @@ class AuthController:
 
     def __init__(self) -> None:
         pass
+
+    def get_current_logged_in_user(self, token: str, secret_key: str, algorithm: str) -> dict[str, Any]:
+        try:
+            payload: dict[str, Any] = jwt.decode(token, secret_key, algorithms=[algorithm])
+            username: str = payload.get("sub")
+            user_id: int | str = payload.get("id")
+            if username is None or user_id is None:
+                raise HttpUnauthorized(detail="Could not validate credentials")
+            return {"username":username, "id":user_id}
+        except JWTError:
+            raise HttpUnauthorized(detail="Invalid token")
 
     def get_user_by_username(self, username:str, session: Session) -> User | None:
         return session.query(User).filter(User.username == username).first()
