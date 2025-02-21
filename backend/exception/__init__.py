6fd9005153f5eb52a7eae395dd0a01637993324f
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-21 21:36:49 +0530

    feat: reorganize exception handling by creating a new module for HTTP exceptions

---
+++ backend/exception/__init__.py
@@ -0,0 +1,5 @@
+from .not_found import HttpNotFound
+from .bad_request import HttpBadRequest
+from .unauthorized import HttpUnauthorized
+from .internal_server_error import HttpInternalServerError
+from .forbidden import HttpForbidden
