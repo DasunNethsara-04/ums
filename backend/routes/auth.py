01256c47bbb1dbddf393447afc7c6658418357e3
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-21 22:12:25 +0530

    feat: add token login and verification endpoints in auth router

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,14 +1,13 @@
 from starlette import status
 from models.users import User
-from jose import JWTError, jwt
 from schema import UserBaseModel
 from typing import Final, Annotated, Any
 from database import get_db, Session
 from fastapi import APIRouter, Depends
-from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from controllers import AuthController, UserController
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from exception import HttpForbidden
 
 router: APIRouter = APIRouter(
     prefix='/auth',
@@ -35,3 +34,15 @@ async def register_user(user: UserBaseModel, db: db_dependency, bcrypt_context: 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]) -> dict[str, Any]:
     return auth_controller.get_current_logged_in_user(token, KEY, ALGORITHM)
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    return auth_controller.login(form_data, db, bcrypt_context, KEY, ALGORITHM)
+
+
+@router.get("/verify-token")
+async def verify_user_token(token: str):
+    if auth_controller.verify_token(token, KEY, ALGORITHM) is None:
+        HttpForbidden(detail="Token is invalid or expired!")
+    return {"detail": "Token is valid"}
