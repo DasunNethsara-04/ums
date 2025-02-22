9337246ac658e2060a2f880a3bd1bb4303afea57
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-22 23:18:53 +0530

    feat: add endpoint to retrieve all users with authentication check

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from database.database import Base, get_db, engine
 from models import User, Role
 from factory import RoleFactory
 from database import DBSeeder
 from routes import auth
+from typing import Annotated
+from sqlalchemy.orm import Session
+from exception import HttpUnauthorized
 
 # initialize the main FastAPI application
 app = FastAPI()
@@ -39,3 +42,11 @@ async def on_shutdown() -> None:
 @app.get("/")
 async def index() -> dict[str, str]:
     return {"message": "Hello, World!"}
+
+
+@app.get("/api/users")
+async def get_all_users(user: Annotated[dict, Depends(auth.get_current_user)], session: Annotated[Session, Depends(get_db)]):
+    if user is None:
+        raise HttpUnauthorized()
+    users = session.query(User).all()
+    return {"users": [user.to_dict() for user in users]}
