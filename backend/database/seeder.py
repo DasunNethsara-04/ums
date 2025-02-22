582bda67b7ae2936a7d4ed8aed252c3dcadb5911
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-22 22:14:15 +0530

    refactor: update DBSeeder to filter existing roles before seeding

--- backend/database/seeder.py
+++ backend/database/seeder.py
@@ -1,15 +1,23 @@
-from typing import Literal, Set
-from pip._vendor.typing_extensions import List
+from sqlalchemy.orm import Session
+from models import Role
 
 
 class DBSeeder:
 
-    def __init__(self, db):
+    def __init__(self, db: Session):
         self.db = db
 
-    def seed(self, data: List|Set) -> Literal[True]:
-        for item in data:
-            self.db.add(item)
-        self.db.commit()
+    def seed(self, data: set[Role]) -> bool:
+        # Query the database for existing role names
+        existing_roles = self.db.query(Role.role).all()
+        existing_roles = {role[0] for role in existing_roles}  # Extract role names
+
+        # Filter out roles that already exist
+        new_roles = [role for role in data if role.role not in existing_roles]
+
+        if new_roles:
+            self.db.add_all(new_roles)
+            self.db.commit()
+
         self.db.close()
         return True
