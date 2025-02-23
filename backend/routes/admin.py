906d3337adb477d5c211ccf9491b531cb3e3f557
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-23 16:53:44 +0530

    feat: add update and delete user endpoints in admin routes

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
-from typing import Annotated, List
-from controllers import AdminController
+from typing import Annotated, List, Any
+from controllers import AdminController, UserController
 from routes import auth
 from sqlalchemy.orm import Session
 from database import get_db
+from models import User
 
 router: APIRouter = APIRouter(
     prefix='/admin',
@@ -12,6 +13,7 @@ router: APIRouter = APIRouter(
 
 # controllers
 admin_controller: AdminController = AdminController()
+user_controller: UserController = UserController()
 
 # dependencies
 db = Annotated[Session, Depends(auth.get_db)]
@@ -22,3 +24,15 @@ user_dep = Annotated[dict, Depends(auth.get_current_user)]
 async def get_all_users(user: user_dep, session: db) -> dict[str, list[dict[str, str | int | bool]]]:
     users = admin_controller.get_all_users(session)
     return {"users": [user.to_dict() for user in users]}
+
+
+# update a user
+@router.put("/{user_id}")
+async def update_user(user: user_dep, user_id: int | str, data: dict[str, Any], db: db):
+    pass
+
+
+# delete a user
+@router.delete("/{user_id}")
+async def delete_user(user: user_dep, user_id: int | str, session: db):
+    return user_controller.delete_user(user_id, session)
