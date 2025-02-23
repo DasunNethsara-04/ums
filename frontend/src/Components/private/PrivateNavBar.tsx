82b4aa4e27066a04317c46c8e2938e136fd1e2e2
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-23 23:42:04 +0530

    feat: enhance PrivateNavBar with logout functionality and Bootstrap styling

--- frontend/src/Components/private/PrivateNavBar.tsx
+++ frontend/src/Components/private/PrivateNavBar.tsx
@@ -1,6 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const logout = () => {
+  localStorage.removeItem('token');
+  window.location.href = '/login';
+};
+
 const PrivateNavBar = () => {
   return (
-    <div>PrivateNavBar</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">UMS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <a className='btn btn-sm btn-outline-danger' onClick={logout}>Logout</a>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
