d5ef3cd63fac7829c2a64404f16d836e2402ca67
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-23 16:33:55 +0530

    feat: add method to retrieve all users in AdminController

--- backend/controllers/admin_controller.py
+++ backend/controllers/admin_controller.py
@@ -1,4 +1,14 @@
+from typing import List
+from models import User
+from sqlalchemy.orm import Session
+
+
 class AdminController:
 
     def __init__(self) -> None:
         pass
+
+    def get_all_users(self, session: Session) -> List[User]:
+
+        users = session.query(User).all()
+        return users
