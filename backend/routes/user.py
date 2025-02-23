45d60265f4704925eafa63197c98c0a0e5f227e4
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-23 16:53:38 +0530

    feat: add update user endpoint in user routes

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from controllers import UserController
-from models import users
+from models import User
 from sqlalchemy.orm import Session
 from typing import Any, Annotated, List
 from database import get_db
@@ -16,6 +16,12 @@ user_controller: UserController = UserController()
 
 # dependencies
 db = Annotated[Session, Depends(get_db)]
+user_dep = Annotated[dict, Depends(auth.get_current_user)]
+
 
 # API endpoints for User
+# update a user
+@router.put("/{user_id}")
+async def update_user(user: user_dep, user_id: int | str, data: dict[str, Any], db: db):
+    pass
 
