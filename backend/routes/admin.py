7efbf8a4d0ed4d889f6d01a2e4ed5bad58603e14
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-24 11:05:19 +0530

    feat: add get_users_count endpoint to retrieve user count with role information

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -4,7 +4,8 @@ from controllers import AdminController, UserController
 from routes import auth
 from sqlalchemy.orm import Session
 from database import get_db
-from models import User
+from exception import HttpUnauthorized
+from routes.auth import oauth2_bearer
 
 router: APIRouter = APIRouter(
     prefix='/admin',
@@ -16,7 +17,7 @@ admin_controller: AdminController = AdminController()
 user_controller: UserController = UserController()
 
 # dependencies
-db = Annotated[Session, Depends(auth.get_db)]
+db = Annotated[Session, Depends(get_db)]
 user_dep = Annotated[dict, Depends(auth.get_current_user)]
 
 
@@ -36,3 +37,9 @@ async def update_user(user: user_dep, user_id: int | str, data: dict[str, Any], 
 @router.delete("/{user_id}")
 async def delete_user(user: user_dep, user_id: int | str, session: db):
     return user_controller.delete_user(user_id, session)
+
+
+@router.get("/users/count")
+async def get_users_count(user: user_dep, session: db) -> dict[str, Any]:
+    count = admin_controller.get_user_count(session)
+    return {"count": count, "role": user['role']}
