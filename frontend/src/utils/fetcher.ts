6966484e87035f0d26ab473cc68cef322e893523
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-24 11:05:26 +0530

    feat: add fetcher utility functions for user and moderator data retrieval

---
+++ frontend/src/utils/fetcher.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosResponse } from "axios"
+
+const fetchUsers = async () => {
+    //
+}
+
+const fetchModerators = async () => {
+    //
+}
+
+const fetchUserCount = async (): Promise<number> => {
+    try {
+        const response: AxiosResponse<{ count: number }> = await axios.get("http://localhost:8000/admin/users/count",
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                },
+            }
+        );
+        if (response.status === 200 && response.data.count) {
+            return response.data.count;
+        } else {
+            throw new Error("Invalid response");
+        }
+    } catch (error) {
+        console.error(error);
+        return 0;
+    }
+}
+
+const fetchModeratorCount = async (): Promise<number> => {
+    try {
+        const response: AxiosResponse<{ count: number }> = await axios.get("http://localhost:8000/admin/moderators/count",
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                },
+            }
+        );
+        if (response.status === 200 && response.data.count) {
+            return response.data.count;
+        } else {
+            throw new Error("Invalid response");
+        }
+    } catch (error) {
+        console.error(error);
+        return 0;
+    }
+}
+
+
+export { fetchUsers, fetchModerators, fetchUserCount, fetchModeratorCount }
