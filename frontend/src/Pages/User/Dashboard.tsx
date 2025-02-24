fd02b33a147aea3b558c2581a6279911eaf66af7
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-24 11:05:55 +0530

    refactor: remove unused role state in AdminDashboard component

--- frontend/src/Pages/User/Dashboard.tsx
+++ frontend/src/Pages/User/Dashboard.tsx
@@ -3,7 +3,6 @@ import AuthChecker from "../../utils/AuthChecker";
 import PrivateNavBar from "../../Components/private/PrivateNavBar";
 
 const AdminDashboard = () => {
-    const [role, setRole] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchRole = async () => {
@@ -11,8 +10,6 @@ const AdminDashboard = () => {
 
             if (!userRole || userRole !== "user") {
                 window.location.href = "/login";
-            } else {
-                setRole(userRole);
             }
         };
 
