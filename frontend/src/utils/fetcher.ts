ed9d837d613e111f74b4284a60b717cc32e24050
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-25 17:17:58 +0530

    feat: enhance fetchUsers to handle errors and validate response

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -1,7 +1,23 @@
 import axios, { AxiosResponse } from "axios"
 
 const fetchUsers = async () => {
-    //
+    try {
+        const response: AxiosResponse = await axios.get("http://localhost:8000/admin/users/",
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            }
+        );
+        if (response.status === 200 && response.data) {
+            return response.data;
+        } else {
+            throw new Error("Invalid response");
+        }
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
 
 const fetchModerators = async () => {
