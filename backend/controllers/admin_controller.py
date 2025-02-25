ac2e9c085b56829685bfc4d00cb1cf5ba266947c
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-25 17:10:12 +0530

    get_all_users() updated to fetch only the role is equal to user

--- backend/controllers/admin_controller.py
+++ backend/controllers/admin_controller.py
@@ -10,7 +10,7 @@ class AdminController:
 
     def get_all_users(self, session: Session) -> List[User]:
 
-        users = session.query(User).all()
+        users = session.query(User).filter(User.role != 'admin').filter(User.disabled == 0).all()
         return users
     
     def get_user_count(self, session: Session) -> int:
