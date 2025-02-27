3052329a0c1dcab2123ea4a1f93ae47f64423d23
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-27 21:43:19 +0530

    get_user_by_id api endpoint implemented | unused imports removed

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated, List, Any
 from controllers import AdminController, UserController
+from models import User
 from routes import auth
 from sqlalchemy.orm import Session
 from database import get_db
-from exception import HttpUnauthorized
-from routes.auth import oauth2_bearer
 
 router: APIRouter = APIRouter(
     prefix='/admin',
@@ -28,13 +27,13 @@ async def get_all_users(user: user_dep, session: db) -> List[dict[str, str | int
 
 
 # update a user
-@router.put("/{user_id}")
+@router.put("/users/{user_id}")
 async def update_user(user: user_dep, user_id: int | str, data: dict[str, Any], db: db):
     pass
 
 
 # delete a user
-@router.delete("/{user_id}")
+@router.delete("/users/{user_id}")
 async def delete_user(user: user_dep, user_id: int | str, session: db):
     return user_controller.delete_user(user_id, session)
 
@@ -42,4 +41,12 @@ async def delete_user(user: user_dep, user_id: int | str, session: db):
 @router.get("/users/count")
 async def get_users_count(user: user_dep, session: db) -> dict[str, Any]:
     count = admin_controller.get_user_count(session)
-    return {"count": count, "role": user['role']}
+    return {"count": count}
+
+
+@router.get("/users/{user_id}")
+async def get_user_by_id(user: user_dep, user_id: int | str, session: db) -> dict[str, str | int | bool] | dict[str, str]:
+    db_user: User = admin_controller.get_user_by_id(session, user_id)
+    if db_user:
+        return db_user.to_dict()
+    return {"detail": "User not found"}
