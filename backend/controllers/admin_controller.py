692414bad9d57bfb9227bcf6a0417e1739ed1da5
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-03-12 20:33:14 +0530

    refactor: implement update and delete user methods with error handling in AdminController

--- backend/controllers/admin_controller.py
+++ backend/controllers/admin_controller.py
@@ -1,6 +1,8 @@
 from typing import List
 from models import User
 from sqlalchemy.orm import Session
+from schema import UserBaseModel
+from exception import HttpNotFound
 
 
 class AdminController:
@@ -18,3 +20,26 @@ class AdminController:
     
     def get_user_by_id(self, session: Session, user_id: int) -> User:
         return session.query(User).filter(User.id == user_id).first()
+
+    def update_user(self, session: Session, user_id: int, form_data: UserBaseModel) -> dict[str, str | int | bool]:
+        db_user: User = session.query(User).filter(User.id == user_id).first()
+        if db_user is None:
+            raise HttpNotFound("User Not Found!")
+        db_user.id = user_id
+        db_user.username = form_data.username
+        db_user.email = form_data.email
+        db_user.name = form_data.name
+        db_user.role = form_data.role
+        db_user.disabled = form_data.disabled
+
+        session.add(db_user)
+        session.commit()
+        return db_user.to_dict()
+
+    def delete_user(self, user_id: int, session: Session) -> bool:
+        user: User = session.query(User).filter(User.id == user_id).first()
+        if user is None:
+            raise HttpNotFound("User Not Found!")
+        session.delete(user)
+        session.commit()
+        return True
