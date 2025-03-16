58e3d5b156bcb893ddd6714777adff962be1a567
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-03-16 23:18:10 +0530

    feat(admin): add endpoint to create a new user and retrieve user count

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated, List, Any
 from controllers import AdminController, UserController
-from exception.not_found import HttpNotFound
+from exception import HttpNotFound
 from models import User
 from routes import auth
 from sqlalchemy.orm import Session
@@ -28,6 +28,12 @@ async def get_all_users(user: user_dep, session: db) -> List[dict[str, str | int
     return [user.to_dict() for user in users]
 
 
+# create a new user
+@router.post("/users/")
+async def create_new_user(user: user_dep, form_data: UserBaseModel, db: db) -> dict[str, Any]:
+    return admin_controller.create_user(form_data, db)
+
+
 # update a user
 @router.put("/users/{user_id}")
 async def update_user(user: user_dep, user_id: int | str, data: UserBaseModel, db: db) -> dict[str, str | int | bool]:
@@ -40,6 +46,7 @@ async def delete_user(user: user_dep, user_id: int | str, session: db) -> bool:
     return admin_controller.delete_user(user_id, session)
 
 
+# get user count
 @router.get("/users/count")
 async def get_users_count(user: user_dep, session: db) -> dict[str, Any]:
     count = admin_controller.get_user_count(session)
