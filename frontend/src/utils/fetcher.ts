d56da50438dad968f5b237dcc509b34f6b363c47
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-03-28 22:08:44 +0530

    fix(fetcher): update fetchUserById and fetchModeratorById to accept string or number as ID

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -79,7 +79,7 @@ const fetchModeratorCount = async (): Promise<number> => {
     }
 }
 
-const fetchUserById = async (id: number): Promise<UserDataInterface | null> => {
+const fetchUserById = async (id: string | number): Promise<UserDataInterface | null> => {
     try {
         const response: AxiosResponse<UserDataInterface, any> = await axios.get(`http://localhost:8000/admin/users/${id}`, {
             headers: {
@@ -97,7 +97,7 @@ const fetchUserById = async (id: number): Promise<UserDataInterface | null> => {
     }
 }
 
-const fetchModeratorById = async (id: number): Promise<UserDataInterface | null> => {
+const fetchModeratorById = async (id: string | number): Promise<UserDataInterface | null> => {
     // Fetch moderator by id
     try {
         const response: AxiosResponse<UserDataInterface, any> = await axios.get(`http://localhost:8000/admin/moderators/${id}`, {
@@ -109,7 +109,6 @@ const fetchModeratorById = async (id: number): Promise<UserDataInterface | null>
             return response.data;
         } else {
             throw new Error("Invalid response");
-            console.log(response.data)
         }
     } catch (err) {
         console.error(err);
